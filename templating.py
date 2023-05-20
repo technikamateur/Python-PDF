b1552494682106a00285102b8e70312ec3c7e562
Daniel <daniel@technikamateur.de>
2023-05-20 13:47:46 +0200

    Repo added

--- templating.py
+++ templating.py
@@ -4,8 +4,15 @@ import json
 from jinja2 import Environment, FileSystemLoader
 import sys
 import argparse
+from collections import defaultdict
 
-default_input = "report.json"
+
+def merge_dicts(d1: dict, d2: dict, d3: dict) -> dict:
+    dd = defaultdict(list)
+    for d in (d1, d2, d3):  # you can list as many input dicts as you want here
+        for key, value in d.items():
+            dd[key].append(value)
+    return dd
 
 
 def main():
@@ -16,6 +23,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', required=False, help='Select input file.')
     args = parser.parse_args()
+    default_input = "report.json"
 
     if args.input:
         in_file = args.input
@@ -24,22 +32,30 @@ def main():
     try:
         with open(in_file, "rt") as json_file:
             data = json_file.read()
-            #data = json.load(json_file)
+            # data = json.load(json_file)
     except FileNotFoundError:
         sys.exit(f"{in_file} not found. Hint: You can specify a file manually with \'-i filename\'.")
-    
+
+    # parsing backslashs and underscores
     data = data.replace("\\", "\\\\textbackslash ")
     data = data.replace("_", "\\\\_")
     data = json.loads(data)
+    # stop if critical errors is not empty
+    if data.get("CRITICAL ERRORS"):
+        sys.exit(f"There were some critical errors. Therefore I\'m not able to generate the report.")
 
     print(f"Keys: {data.keys()}")
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template("example.tex")
 
-    compiler = data["COMPILER INFORMATION"]
-    #lol.update(data["Commits"][0])
-    print(f"Data to jinja: {compiler}")
-    content = template.render(compiler=compiler)
+    compiler = data.get("COMPILER INFORMATION")
+    date = data.get("REPORT INITIALIZATION DATE")
+    repo = data.get("REPOSITORY INFORMATION")
+    commitsmsg = repo.get("Commits").get("commit messages")
+    commitsdate = repo.get("Commits").get("commit dates")
+    commitsbranches = repo.get("Commits").get("commit branches")
+    commits = merge_dicts(commitsmsg, commitsdate, commitsbranches)
+    content = template.render(compiler=compiler, date=date, commits=commits, repo=repo)
 
     with open("out.tex", "wt") as out_file:
         out_file.write(content)

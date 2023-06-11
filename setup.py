93f6a8de203e76a823af01786ea197c3c840c5bd
Daniel <daniel@technikamateur.de>
2023-06-11 16:29:40 +0200

    preparing package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='cieshw_pdf',
+    version='1.0',
+    description='This package can generate pdf reports from cieshw json files.',
+    author='Daniel Körsten',
+    python_requires='>=3.6',
+    install_requires=[
+        'Jinja2>=3.1.2',
+    ],
+    # adding packages
+    packages=find_packages('src'),
+    package_dir ={'':'src'},
+    package_data={'static': ['*'],},
+)

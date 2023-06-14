949f0d607dbc3355d6abea7c03c1363154886b52
Daniel <daniel@technikamateur.de>
2023-06-14 14:56:11 +0200

    fixed static not found

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='cieshw_pdf',
-    version='1.1',
+    version='1.2',
     description='This package can generate pdf reports from cieshw json files.',
     author='Daniel Körsten',
     python_requires='>=3.7',
